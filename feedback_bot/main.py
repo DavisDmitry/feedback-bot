4110224aba97a450a2326a7ca717ecfd84c9d4a9
Dmitry <carlversalski@gmail.com>
2021-07-31 07:49:35 +0500

    💬 Add parsing texts from messages.yml

--- feedback_bot/main.py
+++ feedback_bot/main.py
@@ -15,7 +15,7 @@ DOMAIN = os.environ["BOT_DOMAIN"]
 ADMIN_ID = int(os.environ["ADMIN_ID"])
 DATABASE_PATH = os.environ["DATABASE_PATH"]
 DEFAULT_CHAT_ID = int(os.getenv("DEFAULT_CHAT_ID", ADMIN_ID))
-TEXTS_PATH = os.getenv("TEXTS_PATH", "/data")
+MESSAGES_PATH = os.getenv("MESSAGES_PATH", "/data/messages.yml")
 WH_PATH = "/" + utils.generate_random_string()
 
 
@@ -28,7 +28,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=("start", "help"))
 async def handle_start_and_help_commands(msg: types.Message):
-    await msg.answer(msg.bot.data.get("texts").get("start"))
+    await msg.answer(msg.bot.data["messages"]["start"])
 
 
 @dp.message_handler(
@@ -49,7 +49,7 @@ async def handle_reply_from_admin(msg: types.Message):
     )
     await msg.copy_to(tg_id)
     msg_to_delete = await msg.reply(
-        msg.bot.data.get("texts").get("forwarded_to_user").format(str(tg_id))
+        msg.bot.data["messages"]["forwarded_to_user"].format(user_id=tg_id)
     )
     await asyncio.sleep(5)
     await msg_to_delete.delete()
@@ -62,29 +62,18 @@ async def handle_link_command_from_admin(msg: types.Message):
     try:
         user_id = int(msg.text.split(" ", 1)[1])
     except (IndexError, ValueError):
-        # TODO: add getting text from messages.yml
-        return await msg.reply(
-            "Invalid command. It should be <code>/link <USER ID></code>."
-        )
+        return await msg.reply(msg.bot.data["messages"]["invalid_link_command"])
 
     chat_repo = ChatRepository(msg.bot.data["db_conn"])
 
     if await chat_repo.is_admin_chat(msg.chat.id):
-        # TODO: add getting text from messages.yml
-        return await msg.reply("This chat using for communication with another user.")
+        return await msg.reply(msg.bot.data["messages"]["chat_using_for_another_user"])
 
     if await chat_repo.get_by_user_id(user_id):
-        # TODO: add getting text from messages.yml
-        return await msg.reply(
-            "A chat already exists for communicating with this user."
-        )
+        return await msg.reply(msg.bot.data["messages"]["exist_chat_for_this_user"])
 
     await chat_repo.create(msg.chat.id, user_id)
-    # TODO: add getting text from messages.yml
-    await msg.reply(
-        "The chat has been successfully linked to the user with id "
-        f"<code>{user_id}</code>."
-    )
+    await msg.reply(msg.bot.data["messages"]["chat_linked"].format(user_id=user_id))
 
 
 @dp.message_handler(
@@ -106,7 +95,7 @@ async def handle_message_from_user(msg: types.Message):
     admin_chat_to_forward = await chat_repo.get_by_user_id(msg.chat.id)
     chat_to_forward = admin_chat_to_forward if admin_chat_to_forward else ADMIN_ID
     msg_to_reply = await msg.forward(chat_to_forward)
-    msg_to_delete = await msg.reply(msg.bot.data.get("texts").get("forwarded_to_owner"))
+    msg_to_delete = await msg.reply(msg.bot.data["messages"]["forwarded_to_owner"])
     await msg_to_reply.reply(f"Message from #ID{msg.from_user.id}")
     await asyncio.sleep(5)
     await msg_to_delete.delete()
@@ -125,14 +114,9 @@ async def handle_bot_removed_from_group(cmu: types.ChatMemberUpdated):
 
 
 async def on_startup(dp: Dispatcher):
-    path = TEXTS_PATH.rstrip("/")
-    keys = ("start", "forwarded_to_owner", "forwarded_to_user")
-    dp.bot.data["texts"] = texts = {}
-    for key in keys:
-        with open(f"{path}/{key}.txt", "r") as file:
-            texts[key] = file.read()
+    dp.bot.data["messages"] = utils.parse_messages(MESSAGES_PATH)
 
-    bot.data["db_conn"] = db_conn = sqlite.connect(DATABASE_PATH)
+    dp.bot.data["db_conn"] = db_conn = sqlite.connect(DATABASE_PATH)
     ChatRepository(db_conn).create_table()
 
     await dp.bot.set_webhook(DOMAIN + WH_PATH)
--- feedback_bot/utils.py
+++ feedback_bot/utils.py
@@ -1,6 +1,21 @@
 import random
 import string
 
+import yaml
+
 
 def generate_random_string(length: int = 12) -> str:
     return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def parse_messages(file_path: str) -> dict[str, str]:
+    messages = {}
+    with open(file_path, "r") as file:
+        for key, msg in yaml.safe_load(file).items():
+            if isinstance(msg, list):
+                for index, line in enumerate(msg):
+                    msg[index] = str(line)
+                messages[key] = "\n".join(msg)
+            else:
+                messages[key] = str(msg)
+    return messages
